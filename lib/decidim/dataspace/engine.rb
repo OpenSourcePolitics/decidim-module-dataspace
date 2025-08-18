f98e22e77ec0dcd5601902ac8d9133f5fdf5e3ec
stephanie rousset <strabon@club-internet.fr>
2025-08-18 09:17:05 +0200

    feat: add routes in engine

--- lib/decidim/dataspace/engine.rb
+++ lib/decidim/dataspace/engine.rb
@@ -13,6 +13,24 @@ module Decidim
         # Add engine routes here
         # resources :dataspace
         # root to: "dataspace#index"
+
+        # we want routes like api/v1/data for controllers Api::v1::DataController
+        namespace :api, defaults: { format: :json } do
+          namespace :v1 do
+            scope :data do
+              get "/", to: "data#index"
+              resources :containers, only: [:index, :show], param: :reference
+              resources :authors, only: [:index, :show, :create], param: :reference
+              resources :contributions, only: [:index, :show], param: :reference
+            end
+          end
+        end
+      end
+
+      initializer "dataspace.mount_routes" do
+        Decidim::Core::Engine.routes do
+          mount Decidim::Dataspace::Engine => "/"
+        end
       end
 
       initializer "Dataspace.webpacker.assets_path" do
