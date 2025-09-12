bff77031358148f7bb021aff020b05088b70bee6
stephanie rousset <strabon@club-internet.fr>
2025-09-12 14:54:33 +0200

    feat: get data from application proposals

--- app/controllers/decidim/dataspace/api/v1/data_controller.rb
+++ app/controllers/decidim/dataspace/api/v1/data_controller.rb
@@ -6,50 +6,10 @@ module Decidim
       def index
         render json:
           {
-            containers: Decidim::Dataspace::Container.all.map { |container| render_container(container) },
-            contributions: Decidim::Dataspace::Contribution.all.map { |contribution| render_contribution(contribution) },
-            authors: Decidim::Dataspace::Author.all.map { |author| render_author(author) }
+            containers: Container.from_proposals,
+            contributions: Contribution.from_proposals,
+            authors: Author.from_proposals
           }, status: :ok
-      end
-
-      private
-
-      def render_container(container)
-        {
-          "reference": container.reference,
-          "source": container.source,
-          "name": container.name,
-          "description": container.description,
-          "metadata": container.metadata,
-          "created_at": container.created_at,
-          "updated_at": container.updated_at,
-          "deleted_at": container.deleted_at
-        }
-      end
-
-      def render_contribution(contribution)
-        {
-          "reference": contribution.reference,
-          "source": contribution.source,
-          "container": contribution.container.reference,
-          "locale": contribution.locale,
-          "title": contribution.title,
-          "content": contribution.content,
-          "authors": contribution.authors&.map(&:reference),
-          "metadata": contribution.metadata,
-          "parent": contribution.parent&.reference,
-          "created_at": contribution.created_at,
-          "updated_at": contribution.updated_at,
-          "deleted_at": contribution.deleted_at
-        }
-      end
-
-      def render_author(author)
-        {
-          reference: author.reference,
-          name: author.name,
-          source: author.source
-        }
       end
     end
   end
