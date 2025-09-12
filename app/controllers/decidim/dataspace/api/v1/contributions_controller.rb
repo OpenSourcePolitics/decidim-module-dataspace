1117dbe386bd3bce8a70248503b3af108380f872
stephanie rousset <strabon@club-internet.fr>
2025-09-12 14:54:01 +0200

    feat: get contributions from application proposals

--- app/controllers/decidim/dataspace/api/v1/contributions_controller.rb
+++ app/controllers/decidim/dataspace/api/v1/contributions_controller.rb
@@ -6,37 +6,19 @@ module Decidim
       before_action :set_contribution, only: :show
 
       def index
-        render json: { contributions: Decidim::Dataspace::Contribution.all.map { |contribution| render_contribution(contribution) } }
+        return resource_not_found("Contributions") if Contribution.from_proposals.blank?
+
+        render json: { contributions: Contribution.from_proposals }, status: :ok
       end
 
       def show
-        render json: render_contribution(@contribution)
+        render json: @contribution, status: :ok
       end
 
       private
 
-      def render_contribution(contribution)
-        {
-          reference: contribution.reference,
-          source: contribution.source,
-          container: contribution.container.reference,
-          locale: contribution.locale,
-          title: contribution.title,
-          content: contribution.content,
-          authors: contribution.authors&.map(&:reference),
-          metadata: contribution.metadata,
-          parent: contribution.parent&.reference,
-          created_at: contribution.created_at,
-          updated_at: contribution.updated_at,
-          deleted_at: contribution.deleted_at
-        }
-      end
-
       def set_contribution
-        interoperable = Decidim::Dataspace::Interoperable.find_by(reference: params[:reference])
-        return resource_not_found("Contribution") unless interoperable
-
-        @contribution = Decidim::Dataspace::Contribution.find_by(interoperable_id: interoperable.id)
+        @contribution = Contribution.proposal(params[:reference])
         return resource_not_found("Contribution") unless @contribution
 
         @contribution
--- app/models/decidim/dataspace/contribution.rb
+++ app/models/decidim/dataspace/contribution.rb
@@ -17,6 +17,53 @@ module Decidim
 
       delegate :reference, :source, :metadata, :created_at, :updated_at, :deleted_at, to: :interoperable
 
+      def self.from_proposals
+        Decidim::Proposals::Proposal.includes(:component).all.map do |proposal|
+          {
+            reference: proposal.reference,
+            source: Decidim::ResourceLocatorPresenter.new(proposal).url,
+            container: proposal.component.participatory_space_type.constantize.find(proposal.component.participatory_space_id).reference,
+            locale: "en",
+            title: proposal.title["en"],
+            content: proposal.body["en"],
+            authors: Contribution.authors(proposal),
+            created_at: proposal.created_at,
+            updated_at: proposal.updated_at,
+            deleted_at: nil # does not exist in proposal
+          }
+        end
+      end
+
+      def self.proposal(params_reference)
+        proposal = Decidim::Proposals::Proposal.find_by(reference: params_reference)
+        return nil unless proposal
+
+        {
+          reference: proposal.reference,
+          source: Decidim::ResourceLocatorPresenter.new(proposal).url,
+          container: proposal.component.participatory_space_type.constantize.find(proposal.component.participatory_space_id).reference,
+          locale: "en",
+          title: proposal.title["en"],
+          content: proposal.body["en"],
+          authors: Contribution.authors(proposal),
+          created_at: proposal.created_at,
+          updated_at: proposal.updated_at,
+          deleted_at: nil
+        }
+      end
+
+      def self.authors(proposal)
+        proposal.authors.map do |author|
+          if author.instance_of?(Decidim::User) || author.instance_of?(Decidim::UserGroup)
+            author.name
+          elsif author.instance_of?(Decidim::Organization)
+            author.reference_prefix
+          elsif author.instance_of?(Decidim::Meetings::Meeting)
+            author.reference
+          end
+        end
+      end
+
       private
 
       def title_or_content
