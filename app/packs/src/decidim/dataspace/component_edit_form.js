79a6a1e0578708b95508461cfe0e32e82eda28f2
stephanie rousset <strabon@club-internet.fr>
2025-10-17 11:54:42 +0200

    feat: show/hide preferred locale and add placeholder for url

--- app/packs/src/decidim/dataspace/component_edit_form.js
+++ app/packs/src/decidim/dataspace/component_edit_form.js
@@ -1,17 +1,25 @@
 document.addEventListener("DOMContentLoaded", function(){
     const integrationCheck = document.querySelector('input#component_settings_add_integration')
     const urlDiv = document.querySelector("div.integration_url_container")
+    const localeDiv = document.querySelector("div.preferred_locale_container")
+    const inputUrl = document.querySelector("input[name='component[settings][integration_url]']")
+    inputUrl.setAttribute("placeholder", "https://platform.com")
+
     if(integrationCheck){
         if(integrationCheck.checked){
             urlDiv.style.display = "block"
+            localeDiv.style.display = "block"
         } else {
             urlDiv.style.display = "none"
+            localeDiv.style.display = "none"
         }
         integrationCheck.addEventListener('change', function(){
             if (this.checked) {
                 urlDiv.style.display = "block"
+                localeDiv.style.display = "block"
             } else {
                 urlDiv.style.display = "none"
+                localeDiv.style.display = "none"
             }
         })
     }
